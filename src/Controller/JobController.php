dbf7a4b8001e11e74b991b0d202c124c9fe4b45e
rossoromain <bif.rosso@gmail.com>
2022-01-05 13:58:57 +0100

    mise en commun

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -24,7 +24,7 @@ class JobController extends AbstractController
     }
 
     // liste des jobs
-    #[Route('/list', name: 'job_list')]
+    #[Route('/', name: 'job_list')]
     public function list(): Response
     {
         $repository = $this->registryManager->getManager()->getRepository(Job::class);
