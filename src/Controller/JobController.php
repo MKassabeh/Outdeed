58ac222d21945e7f124013f8f1a7b06047378711
rossoromain <bif.rosso@gmail.com>
2022-01-05 14:34:28 +0100

    Crud fini sauf EDIT

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -16,8 +16,40 @@ class JobController extends AbstractController
 
     private $registryManager;
 
+    public $contractType = [
+        'CDI - Temps plein',
+        'CDI - Temps partiel',
+        'CDD - Temps plein',
+        'CDD - Temps partiel',
+        'Interim',
+        'Apprentissage',
+        'Stage',
+        'Contrat professionnel',
+        'CDI de chantier ou d\'opérations'
+    ];
+
     public $categories = [
-        '' => '',
+        'Agriculture',
+        'Alimentation',
+        'Animaux',
+        'Architecture',
+        'Services publics',
+        'Banque',
+        'Biologie',
+        'BTP',
+        'Cinema',
+        'Immobilier',
+        'Communication',
+        'Culture',
+        'Droit',
+        'Electricité',
+        'Hôtellerie-Restauration',
+        'Informatique',
+        'Mécanique',
+        'Santé',
+        'Sport',
+        'Secretariat',
+        'Transport-Logistique'
     ]; 
 
     public function __construct(ManagerRegistry $registryManager) {
@@ -52,12 +84,6 @@ class JobController extends AbstractController
     #[Route('/add', name: 'job_add')]
     public function add(): Response
     {
-        $categories_availables = [
-            'property'  => 'Immobilier',
-            'clothes'   => 'Vêtements',
-            'cars'      => 'Voitures',
-            'holidays'  => 'Vacances',
-        ];
         $errors = [];
         $formIsValid = null;
 
@@ -65,48 +91,54 @@ class JobController extends AbstractController
             $safe = array_map('trim', array_map('strip_tags', $_POST));
 
                 // Vérif titre
-                if(strlen($safe['title']) < 5 || strlen($safe['title']) > 50){
-                    $errors[] = 'Votre titre doit comporter entre 5 et 50 caractères';
+                if(strlen($safe['title']) < 5 || strlen($safe['title']) > 100){
+                    $errors[] = 'Votre titre doit comporter entre 5 et 100 caractères';
                 }
 
                 // Vérif catégorie
                 if(!isset($safe['category'])){
                     $errors[] = 'Veuillez sélectionner une catégorie';
                 }
-                elseif(!in_array($safe['category'], array_keys($categories_availables))){
+                elseif(!in_array($safe['category'], $this->categories)){
                     $errors[] = 'Votre catégorie sélectionnée n\'existe pas';
                 }
 
-                // Vérif description
-                if(strlen($safe['description_company']) < 10 || strlen($safe['description_company']) > 500){
-                    $errors[] = 'Votre description de company doit comporter entre 10 et 500 caractères';
+                // Vérif description entreprise
+                if(strlen($safe['description_company']) < 1){
+                    $errors[] = 'Veuillez entrer la description de l\'entreprise';
                 }
-                // Vérif description
-                if(strlen($safe['description_job']) < 10 || strlen($safe['description_job']) > 500){
-                    $errors[] = 'Votre description du job doit comporter entre 10 et 500 caractères';
+                // Vérif description emploi
+                if(strlen($safe['description_job']) < 1){
+                    $errors[] = 'Veuillez entrer la description de l\'emploi';
                 }
-                // Vérif description
-                if(strlen($safe['description_applicant']) < 10 || strlen($safe['description_applicant']) > 500){
-                    $errors[] = 'Votre description de profil doit comporter entre 10 et 500 caractères';
+                // Vérif description recherche
+                if(strlen($safe['description_applicant']) < 1){
+                    $errors[] = 'Veuillez entrer la description du profil recherché';
                 }
-                // Vérif description
-                if(strlen($safe['city']) < 10 || strlen($safe['city']) > 500){
-                    $errors[] = 'Votre salaire doit comporter entre 10 et 500 caractères';
+                // Vérif city
+                if(strlen($safe['city']) < 1 || strlen($safe['city']) > 100){
+                    $errors[] = 'Votre salaire doit comporter entre 1 et 100 caractères';
                 }
-                if(strlen($safe['wages']) < 10 || strlen($safe['wages']) > 500){
-                    $errors[] = 'Votre salaire doit comporter entre 10 et 500 caractères';
+
+                // Vérif salaires
+                if(strlen($safe['wages']) < 1 || strlen($safe['wages']) > 50){
+                    $errors[] = 'Votre salaire doit comporter entre 1 et 50 caractères';
                 }
-                // Vérif description
-                if(strlen($safe['contract']) < 10 || strlen($safe['contract']) > 500){
-                    $errors[] = 'Votre contrat doit comporter entre 10 et 500 caractères';
+                
+                // Vérif type contrat
+                if(!isset($safe['contract'])){
+                    $errors[] = 'Veuillez sélectionner un type de contrat';
                 }
-                // Vérif description
-                if(strlen($safe['schedule']) < 10 || strlen($safe['schedule']) > 500){
-                    $errors[] = 'Votre horaire doit comporter entre 10 et 500 caractères';
+                elseif(!in_array($safe['contract'], $this->contractType)){
+                    $errors[] = 'Votre type de contrat n\'existe pas';
                 }
-                // Vérif description
-                if(strlen($safe['comment']) < 10 || strlen($safe['comment']) > 500){
-                    $errors[] = 'Votre commentaire doit comporter entre 10 et 500 caractères';
+                // Vérif horaires
+                if(strlen($safe['schedule']) > 255){
+                    $errors[] = 'Vos indications concernant les horaires ne doivent pas dépasser 255 caractères.';
+                }
+                // Vérif commentaire
+                if(strlen($safe['comment']) > 500){
+                    $errors[] = 'Votre commentaire doit comporter moins de 500 caractères';
                 }
 
                 if(count($errors) === 0){
@@ -117,42 +149,36 @@ class JobController extends AbstractController
     
     
                     // Equivalent de notre INSERT INTO et bindValue()
-                    $annonce = new Job(); // Appelle de l'entity Annonces 
+                    $job = new Job(); // Appelle de l'entity job 
                     // On enregistre chaque valeur
-                    $annonce->setTitle($safe['title']);
-                    $annonce->setCategory($safe['category']);
-                    $annonce->setDescriptionCompany($safe['description_company']);
-                    $annonce->setDescriptionApplicant($safe['description_applicant']);
-                    $annonce->setDescriptionJob($safe['description_job']);
-                    $annonce->setWages($safe['wages']);
-                    $annonce->setCity($safe['city']);
-                    $annonce->setContractType($safe['contract']);
-                    $annonce->setPublishedAt(new \DateTime('now'));
-                    $annonce->setSchedule($safe['schedule']);
-                    $annonce->setCompanyComment($safe['comment']);
+                    $job->setTitle($safe['title']);
+                    $job->setCategory($safe['category']);
+                    $job->setDescriptionCompany($safe['description_company']);
+                    $job->setDescriptionApplicant($safe['description_applicant']);
+                    $job->setDescriptionJob($safe['description_job']);
+                    $job->setWages($safe['wages']);
+                    $job->setCity($safe['city']);
+                    $job->setContractType($safe['contract']);
+                    $job->setPublishedAt(new \DateTime('now'));
+                    $job->setSchedule($safe['schedule']);
+                    $job->setCompanyComment($safe['comment']);
     
-    
-    
-    
-                     // Equivalent à notre execute()
-                    $em->persist($annonce);
+                    // Equivalent à notre execute()
+                    $em->persist($job);
                     $em->flush(); // On libère la base de données (elle arrete d'être en attente de quelque chose);
     
                     // Envoi d'un message flash
-                    $this->addFlash('success','Bravo, votre annonce a bien été enregistrée');
-    
+                    $this->addFlash('success','Bravo, votre offre d\'emploi a bien été enregistrée');  
     
                 }
                 else { // Ici j'ai des erreurs et j'affiche celle-ci
                     $this->addFlash('danger', implode('<br>', $errors));
                 }
     
-            } 
-
-
-        
+            }         
             return $this->render('job/add.html.twig', [
-                'categories_availables' => $categories_availables,
+                'categories_availables' => $this->categories,
+                'contract_type'         => $this->contractType
     
             ]);
     }
