4b862257f69e7b151e8010aa3325067f463e3c98
rossoromain <bif.rosso@gmail.com>
2022-01-13 16:20:35 +0100

    Update ApplyController.php

--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -26,9 +26,10 @@ class ApplyController extends AbstractController
     }
 
     #[Route('/apply/{id}', name: 'apply')]
-    public function index(int $id, MailerInterface $mailer
-    ): Response
-    {
+    public function index(
+        int $id,
+        MailerInterface $mailer
+    ): Response {
 
         $em = $this->registryManager->getManager();
         $job = $em->getRepository(Job::class)->find($id);
@@ -39,34 +40,34 @@ class ApplyController extends AbstractController
 
             $safe = array_map('trim', array_map('strip_tags', $_POST));
 
-                // Vérif titre
-                if(strlen($safe['motivation_letter']) < 5 || strlen($safe['motivation_letter']) > 100){
-                    $errors[] = 'Votre titre doit comporter entre 5 et 100 caractères';
-                }                
+            // Vérif titre
+            if (strlen($safe['motivation_letter']) < 5 || strlen($safe['motivation_letter']) > 100) {
+                $errors[] = 'Votre titre doit comporter entre 5 et 100 caractères';
+            }
 
-                if(count($errors) === 0){
-                    // ici, je n'ai pas d'erreur, j'enregistre en base de données
-    
-                    $em = $this->registryManager->getManager(); // Connexion à la bdd (équivalent new PDO()) sans oublier le paramètre de la fonction ManagerRegistry $doctrine et le "use"
-    
- 
-                    $job_offer = $em->getRepository(Job::class)->find($id);
-                    $job_apply = new Apply(); 
+            if (count($errors) === 0) {
+                // ici, je n'ai pas d'erreur, j'enregistre en base de données
 
-                    $job_apply->setMotivationLetter($safe['motivation_letter']);
+                $em = $this->registryManager->getManager(); // Connexion à la bdd (équivalent new PDO()) sans oublier le paramètre de la fonction ManagerRegistry $doctrine et le "use"
 
-                    $job_apply->setUser($this->getUser());
-                    $job_apply->setJob($job_offer);
 
-    
-                    // Equivalent à notre execute()
-                    $em->persist($job_apply);
-                    $em->flush(); // On libère la base de données (elle arrete d'être en attente de quelque chose);
-    
-                    // Envoi du mail
-                    $company_name = $job_offer->getCompanyName();  
-                    $company_email = $em->getRepository(Company::class)->findOneBy(['name' => $company_name])->getContactEmail();
-                    $email = (new Email())
+                $job_offer = $em->getRepository(Job::class)->find($id);
+                $job_apply = new Apply();
+
+                $job_apply->setMotivationLetter($safe['motivation_letter']);
+
+                $job_apply->setUser($this->getUser());
+                $job_apply->setJob($job_offer);
+
+
+                // Equivalent à notre execute()
+                $em->persist($job_apply);
+                $em->flush(); // On libère la base de données (elle arrete d'être en attente de quelque chose);
+
+                // Envoi du mail
+                $company_name = $job_offer->getCompanyName();
+                $company_email = $em->getRepository(Company::class)->findOneBy(['name' => $company_name])->getContactEmail();
+                $email = (new Email())
                     ->from('notifications@outdeed.com')
                     ->to($company_email)
                     //->cc('cc@example.com')
@@ -77,19 +78,78 @@ class ApplyController extends AbstractController
                     ->text('un candidat à postuler à cette offre!')
                     ->html('<p>Un candidat vient de postuler !</p>');
 
-                    $mailer->send($email);
+                $mailer->send($email);
 
 
 
-                    // Envoi d'un message flash
-                    $this->addFlash('success','Bravo, votre candidature a bien été envoyée');  
-                }
-                else { // Ici j'ai des erreurs et j'affiche celle-ci
-                    $this->addFlash('danger', implode('<br>', $errors));
-                }
-            } 
+                // Envoi d'un message flash
+                $this->addFlash('success', 'Bravo, votre candidature a bien été envoyée');
+            } else { // Ici j'ai des erreurs et j'affiche celle-ci
+                $this->addFlash('danger', implode('<br>', $errors));
+            }
+        }
         return $this->render('apply/index.html.twig', [
             'controller_name' => 'ApplyController',
+        ]);
+    }
+
+    #[Route('/account/applicants', name: 'offer_applicant')]
+    #[IsGranted('ROLE_USER')]
+    public function applicants(): Response
+    {
+
+        // Si l'user est ni une company ni un admin -> redirection
+        if ($this->getUser()->getUserType() != 'company' && !in_array("ROLE_ADMIN", $this->getUser()->getRoles())) {
+            $this->addFlash('warning', 'Cette page est reservée aux entreprises publiant des offres d\'emplois');
+            return $this->redirectToRoute('job_list');
+        }
+
+        $em = $this->registryManager->getManager();
+
+        // On récupère les offres de l'entreprise
+        $offers = $em->getRepository(Job::class)->findBy(['published_by' => $this->getUser()]);
+
+        $postulations = [];
+
+
+        // On boucle sur les offres pour récupérer les candidatures
+        foreach ($offers as $offer) {
+            $postulations[] = [
+                'job'        => $offer,
+                'applicants' => $em->getRepository(Apply::class)->findBy(['job' => $offer->getId()]),
+            ];
+        }
+
+        // on cherche les fiches candidat associées
+        $list = [];
+        foreach ($postulations as $post) {
+            foreach ($post['applicants'] as $app) {
+                $post['candidates'][] = $em->getRepository(Candidate::class)->findOneBy(['user' => $app->getUser()]);
+            }
+            $list[] = $post;
+        }
+
+        // foreach ($applicants[0] as $app) {
+        //     $candidate[] = $em->getRepository(Candidate::class)->findBy(['user' => $app->getUser()]);            
+        // }     
+
+        // dd($applicants, $candidate);
+
+        // PROBLEME :
+
+        // Je récupère les offres postés par l'entreprise connectée (stockées dans $offers) [Ligne 90]
+        // Pour chaque offre de $offers, je stocke dans le tableau applicants[] les lignes de Apply (la/les postulations) 
+        // -> donc $applicants[0] contient toutes les postulations pour l'offre $offers[0] [Ligne 92/94]
+        // Ensuite pour chaque élément de l'array $applicants[]
+        // je stock dans un array $candidate[] la fiche candidat associé a lapostulation [Ligne 96/98]
+
+        // problème : c'est le bordel, je me perds, j'ai deux tableaux dégeulasses contenant eux-même des tableaux
+        // au secours (voir le dd en dessous)
+
+
+        return $this->render('apply/view.html.twig', [
+            'postulations' => $list,
+
         ]);
     }
 }
